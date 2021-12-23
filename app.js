f45f0bbfdd23d2b591469bb5a03e597ddd815668
ahmed-alhomsi <ahmed.alhomsi.official@gmail.com>
2021-12-23 14:47:32 +0200

    added a background image.

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ switchMode.addEventListener('click', (e)=>{
         darkMode = false;
         lightIcon.classList.add('d-none');
         darkIcon.classList.remove('d-none');
+        document.querySelector('.bg-custom-dark').classList.add('bg-custom-light');
+        document.querySelector('.bg-custom-dark').classList.remove('bg-custom-dark');
         mainElements.forEach(element=>{
             element.classList.remove('bg-dark-main');
             element.classList.add('bg-light-main');
@@ -19,6 +21,8 @@ switchMode.addEventListener('click', (e)=>{
         darkMode = true;
         lightIcon.classList.remove('d-none');
         darkIcon.classList.add('d-none');
+        document.querySelector('.bg-custom-light').classList.add('bg-custom-dark');
+        document.querySelector('.bg-custom-light').classList.remove('bg-custom-light');
         mainElements.forEach(element=>{
             element.classList.add('bg-dark-main');
             element.classList.remove('bg-light-main');
